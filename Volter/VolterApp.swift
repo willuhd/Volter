610a600679ea8ada7ad414e06ee4bbf93a736233
willuhd <willcxd@outlook.com>
2026-07-05 18:22:08 +0800

    add basic GUI

---
+++ Volter/VolterApp.swift
@@ -0,0 +1,55 @@
+//
+//  VolterApp.swift
+//  Volter
+//
+//  Created by Will on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct VolterApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    var popover: NSPopover?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Disable the Dock icon
+        NSApp.setActivationPolicy(.accessory)
+
+        // Setup popover window
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 290, height: 125)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: ContentView())
+        self.popover = popover
+
+        // Setup Menu Bar Item with lightning bolt symbol
+        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = self.statusItem?.button {
+            button.image = NSImage(systemSymbolName: "bolt.fill", accessibilityDescription: "Volter")
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+
+    @objc func togglePopover(_ sender: AnyObject?) {
+        guard let button = statusItem?.button else { return }
+        if let popover = popover {
+            if popover.isShown {
+                popover.performClose(sender)
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+                popover.contentViewController?.view.window?.makeKey()
+            }
+        }
+    }
+}
